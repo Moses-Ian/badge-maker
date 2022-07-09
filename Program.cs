08f10af874d2a70ddfc2d6bf1c22695f0a5923b2
Expired <imoses2@hotmail.com>
2022-07-09 16:13:48 -0700

    learned the fundamentals

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CatWorx.BadgeMaker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+			string[] favFoods = new string[3]{ "pizza", "doughnuts", "icecream" };
+			string firstFood = favFoods[0];
+			string secondFood = favFoods[1];
+			string thirdFood = favFoods[2];
+			Console.WriteLine("I like {0}, {1}, and {2}", favFoods[0], favFoods[1], favFoods[2]);
+        }
+    }
+}
