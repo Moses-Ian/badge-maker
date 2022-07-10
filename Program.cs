178eb90528188af75851b8f6b57bd84a92038c81
Expired <imoses2@hotmail.com>
2022-07-09 19:30:45 -0700

    learned about web/images

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ namespace CatWorx.BadgeMaker
 			List<Employee> employees = GetEmployees();
 			Util.PrintEmployees(employees);
 			Util.MakeCSV(employees);
+			Util.MakeBadges(employees);
 		}
 		
 		static List<Employee> GetEmployees()
@@ -27,8 +28,9 @@ namespace CatWorx.BadgeMaker
 				string lastName = Console.ReadLine();
 				Console.WriteLine("Please enter an id: ");
 				int id = Int32.Parse(Console.ReadLine());
-				Console.WriteLine("Please enter a photo url: ");
-				string photoUrl = Console.ReadLine();
+				// Console.WriteLine("Please enter a photo url: ");
+				// string photoUrl = Console.ReadLine();
+				string photoUrl = "https://placekitten.com/300/300";
 				
 				Employee currentEmployee = new Employee(firstName, lastName, id, photoUrl);
 				employees.Add(currentEmployee);
